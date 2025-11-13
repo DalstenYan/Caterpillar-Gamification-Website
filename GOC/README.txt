ade2dee6133827b3ccd591a788689f12b5a59ee7
Mungarox <mungaro@mail.bradley.edu>
2025-11-13 08:24:00 -0600

    Node.Js integration

---
+++ GOC/README.txt
@@ -0,0 +1,13 @@
+Node.js installation is required
+
+Dependencies: Run the following command in the same directory as Server.js (should be GOC) to install dependencies
+npm install express mysql2 dotenv
+
+.env example
+.env contains the log in info that feeds into Server.js. For security/best-practice it has been partially redacted. The information is the same as posted in Discord.
+
+Connect to server
+Run the following command in the same directory as Server.js (should be GOC)
+node server.js
+
+
---
+++ GOC/Server.js
@@ -0,0 +1,55 @@
+﻿import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = 3306;
+
+app.use(express.static("public"));
+
+// Database connection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+// Test connection
+db.connect(err => {
+    if (err) {
+        console.error("Database connection failed:", err);
+        return;
+    }
+    console.log("Connected to server successfully!");
+});
+
+// POST 
+app.post('/login', (req, res) => {
+    const { CWS, password } = req.body;
+
+    if (!CWS || !password) {
+        return res.status(400).json({ success: false, message: 'Missing credentials' });
+    }
+    //Login 
+    const sql = 'SELECT * FROM employee WHERE CWS_ID = ? AND Password = ?';
+    db.query(sql, [CWS, password], (err, results) => {
+        if (err) {
+            console.error('Error during query:', err);
+            return res.status(500).json({ success: false, message: 'Database error' });
+        }
+
+        if (results.length > 0) {
+            res.json({ success: true, message: 'Login successful' });
+        } else {
+            res.status(401).json({ success: false, message: 'Invalid CWS or password' });
+        }
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
